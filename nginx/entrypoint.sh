d94e22fd236bbf9455fd33bb0c1614af9e99602c
Alisher Radjabov <alisherradjabovracer@gmail.com>
2024-08-26 16:45:41 +0500

    pipeline fix

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 if [ ! -f /etc/letsencrypt/live/ali-nek.lol/fullchain.pem ]; then
-    certbot --nginx -d ali-nek.lol -d www.ali-nek.lol --non-interactive --agree-tos --no-eff-email alisherradjabovracer@gmail.com
+    certbot --nginx -d ali-nek.lol -d www.ali-nek.lol --non-interactive --agree-tos --email $CERTBOT_EMAIL
 fi
 
 exec nginx -g 'daemon off;'
+
