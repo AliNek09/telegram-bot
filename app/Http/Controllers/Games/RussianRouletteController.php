893384001d8c9451ad53c7bbcb1947ea2aa6a444
Alisher Radjabov <alisherradjabovracer@gmail.com>
2024-08-29 08:43:34 +0500

    test deploy

--- app/Http/Controllers/Games/RussianRouletteController.php
+++ app/Http/Controllers/Games/RussianRouletteController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Games;
 use App\Http\Requests\Games\RussianRouletteRequest;
 use App\Models\Games\RussianRoulette;
 use App\Services\WebHookService;
+use Illuminate\Support\Facades\Log;
 
 class RussianRouletteController
 {
@@ -21,16 +22,19 @@ class RussianRouletteController
         $data = $request->validateData();
         $result = ($data['number'] == rand(0,10));
         if($result) {
-            $this->hook->sendMessage($data['chat_id'], 'BAANNGG!!!');
+            $response = $this->hook->sendMessage($data['chat_id'], 'BAANNGG!!!');
         } else {
-            $this->hook->sendMessage($data['chat_id'], 'You are lucky. You missed the shot.');
+            $response = $this->hook->sendMessage($data['chat_id'], 'You are lucky. You missed the shot.');
         }
+
+        Log::channel('telegram')->info('Telegram API Request:', [
+            'endpoint' => 'sendMessage',
+            'payload' => ['chat_id' => $data['chat_id'], 'text' => $result ? 'BAANNGG!!!' : 'You are lucky. You missed the shot.'],
+            'response_status' => $response->status(),
+            'response_body' => $response->body()
+        ]);
 
         $data['result'] = $result;
         RussianRoulette::query()->create($data);
-
-        return response()->json([
-            'message' => 'ok'
-        ], 200);
     }
 }
